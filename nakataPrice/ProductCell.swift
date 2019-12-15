914597b62e5c2081634ee2b6b6099b3c8f41f4c4
Mikio <21nakatam@gmail.com>
2019-12-15 22:21:11 +0900

    First commit

---
+++ nakataPrice/ProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCell.swift
+//  nakataPrice
+//
+//  Created by Mikio Nakata on 12/15/19.
+//  Copyright © 2019 Mikio Nakata. All rights reserved.
+//
+
+// Product Cell (For tableview)
+//
+
+import UIKit
+
+class productCell: UITableViewCell {
+    @IBOutlet weak var nameProduct: UILabel!
+    @IBOutlet weak var brandProduct: UILabel!
+    @IBOutlet weak var sizeProduct: UILabel!
+    @IBOutlet weak var colorProduct: UILabel!
+    
+    
+    @IBOutlet weak var quantity: UILabel!
+    
+}
---
+++ nakataPrice/SalesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SalesViewController.swift
+//  nakataPrice
+//
+//  Created by Mikio Nakata on 12/15/19.
+//  Copyright © 2019 Mikio Nakata. All rights reserved.
+//
+
+import UIKit
+
+class SalesViewController: UIViewController {
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
+
