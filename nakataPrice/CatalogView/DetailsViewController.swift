712b5abb5a72ed0582d73e2f6cfe5ede2d9a75aa
Mikio <21nakatam@gmail.com>
2020-03-16 16:00:14 +0900

    Change

--- nakataPrice/CatalogView/DetailsViewController.swift
+++ nakataPrice/CatalogView/DetailsViewController.swift
@@ -9,15 +9,39 @@
 import Foundation
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData = [["color", "color", "color", "color"],
+                      ["size", "size", "size", "size"],
+                      ["quantity", "quantity", "quantity", "quantity"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        self.picker.delegate = self
+        self.picker.dataSource = self
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerData.count
+    }
     
+    // Count of picker column
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+    
+    // Capture the picker selection view
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // Triggered when user makes change to picker collection.
+        // Parameters named row and component determines what was selected.
+    }
     
 }
